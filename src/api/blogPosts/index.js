c1b5bd9e543995366f623f5efbb2434ed4b13944
Louis Gadza <siuolgadza@gmail.com>
2022-12-21 15:01:19 +0100

    working APIs

--- src/api/blogPosts/index.js
+++ src/api/blogPosts/index.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import uniqid from "uniqid";
 import fs from "fs";
 import httpErrors from "http-errors";
+import { title } from "process";
 
 const { NotFound, Unauthorised, BadRequest } = httpErrors;
 
@@ -29,3 +30,75 @@ blogPostsRouter.post("/", (req, res, next) => {
   writeBlogPosts(blogPostsList);
   res.status(201).send({ _id: newBlogPost._id });
 });
+blogPostsRouter.get("/", (req, res, next) => {
+  const blogPostsList = getBlogPosts();
+
+  if ((req.query && req.query.title) || req.query.category) {
+    blogPostsList.filter(
+      (blogPost) =>
+        blogPost.title === req.query.title ||
+        blogPost.category === req.query.category
+    );
+  } else {
+    res.send(blogPostsList);
+  }
+});
+blogPostsRouter.get("/:blogPostId", (req, res, next) => {
+  try {
+    const blogPostsList = getBlogPosts();
+    const foundBlogPost = blogPostsList.find(
+      (blogPost) => blogPost._id === req.params.blogPostId
+    );
+    if (foundBlogPost) {
+      res.send(foundBlogPost);
+    } else {
+      next(NotFound(`Blog Post id ${req.params.blogPostId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+blogPostsRouter.put("/:blogPostId", (req, res, next) => {
+  try {
+    console.log(req.body);
+    const blogPostsList = getBlogPosts();
+    const index = blogPostsList.findIndex(
+      (blogPost) => blogPost._id === req.params.blogPostId
+    );
+    if (index !== -1) {
+      const oldBlogPost = blogPostsList[index];
+
+      const editedBlogPost = {
+        ...oldBlogPost,
+        ...req.body,
+        updatedAt: new Date(),
+      };
+      blogPostsList[index] = editedBlogPost;
+      writeBlogPosts(blogPostsList);
+      res.send(editedBlogPost);
+    } else {
+      next(NotFound(`Blog Post with id ${req.params.blogPostId} not found`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+blogPostsRouter.delete("/:blogPostId", (req, res, next) => {
+  try {
+    const blogPostList = getBlogPosts();
+    const remainingBlogPost = blogPostList.filter(
+      (blogPost) => blogPost._id !== req.params.blogPostId
+    );
+    if (blogPostList.length !== remainingBlogPost.length) {
+      writeBlogPosts(remainingBlogPost);
+      res.status(204).send();
+    } else {
+      next(
+        NotFound(`The Blog Post with id ${req.params.blogPostId} not found :(`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+export default blogPostsRouter;
--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 // import { authorsRouter } from "./api/authors/index.js";
 import authorsRouter from "./api/authors/index.js";
 import listEndpoints from "express-list-endpoints";
+import blogPostsRouter from "./api/blogPosts/index.js";
 
 const server = express();
 const port = 3001;
 server.use(express.json());
 
 server.use("/authors", authorsRouter);
+server.use("/blogPosts", blogPostsRouter);
 
 server.listen(port, () => {
   console.table(listEndpoints(server));
