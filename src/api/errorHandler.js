1a3c3898368b38f794903527f0eef8e55ec5f7f8
Louis Gadza <siuolgadza@gmail.com>
2023-01-10 15:45:23 +0100

    change generic Error handler

--- src/api/errorHandler.js
+++ src/api/errorHandler.js
@@ -25,7 +25,5 @@ export const unauthorizedHandler = (err, req, res, next) => {
 export const genericErrorHandler = (err, req, res, next) => {
   console.log("generic error", err);
 
-  res
-    .status(500)
-    .send({ message: "An error occurred on our side  we are working on it" });
+  res.status(500).send({ message: err.message });
 };
